8eea3bf190fc922cb86f560a81ce9d896690ea27
Aanandita <aanandita.pujji@gmail.com>
2025-07-18 10:58:58 -0400

    create skeleton html

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
-const operation = (x,y,operation) => {
+const add = (x,y) => {
+    return x + y;
+}
+
+const subtract = (x,y) => {
+    return x - y;   
+}
+
+const multiply = (x,y) => {
+    return x * y;
+}
+
+const divide = (x,y) => {
+    return x / y;
+}
+
+const operate = (x,y,operation) => {
     switch (operation) {
-        case "+":
-            return x = y;
-        case "-":
-            return x - y;
-        case "*":
-            return x * y;
-        case "/":
-            return x / y;
+        case "add":
+            add(x,y);
+        case "subtract":
+            subtract(x,y);
+        case "multiply":
+            multiply(x,y);
+        case "divide":
+            divide(x,y);
+        default:
+            console.error("not a valid operation");
     }
 }
