e7ec0e732e0e79697ec7ce244e1ff25d24e62af8
Aanandita <aanandita.pujji@gmail.com>
2025-07-20 14:51:15 -0400

    final logic/code cleanup

--- js/script.js
+++ js/script.js
@@ -1,117 +1,98 @@
-const add = (x,y) => {
-    return x + y;
-}
+const add = (x,y) => x + y;
+const subtract = (x,y) => x - y;
 
-const subtract = (x,y) => {
-    return x - y;   
-}
-
-const multiply = (x,y) => {
-    return x * y;
-}
-
-const divide = (x,y) => {
-    return x / y;
-}
+const multiply = (x,y) => x * y;
+const divide = (x, y) => (y === 0 ? "nice try" : x / y)
 
 const operate = (x,operator,y) => {
     x = parseFloat(x)
     y = parseFloat(y);
     switch (operator) {
-        case "+":
-            return add(x,y);
-        case "-":
-            return subtract(x,y);
-        case "×":
-            return multiply(x,y);
-        case "÷":
-            return divide(x,y);
+        case "+": return add(x,y);
+        case "-": return subtract(x,y);
+        case "×": return multiply(x,y);
+        case "÷": return divide(x,y);
         default:
-            console.error("not a valid operation");
+            updateDisplay("err");
+            return "Error";
     }
 }
 
 const updateDisplay = (content) => {
-    console.log(content);
-    const display = document.querySelector("#display");
-    display.textContent = content;
+    document.querySelector("#display").textContent = content;
 };
 
+const getDisplayContent = (operand1, operator, operand2) => {
+    return `${operand1}${operator ? " " + operator : ""}${operand2 ? " " + operand2 : ""}`;
+}
+
 const init = () => {
-    const display = document.querySelector("#display");
     const buttons = document.querySelector("#buttons");
     let operand1 = "", operator = "", operand2 = "";
-    let resetCalculator = false;
+    let newCalculation = false;
+
+    const resetState = () => {
+        operand1 = "";
+        operator = "";
+        operand2 = "";
+        newCalculation = false;
+    }
     
     buttons.addEventListener("click", (event) => {
         const target = event.target;
-        if (target.id === "buttons") return;
-        switch (true) {
-            case target.classList.contains("digit"):
-                if (operator.length) {
-                    if (target.value === "." && operand2.includes(".")) return;
-                    if (target.value !== "." && operand2[operand2.length-1] === "0") return;
-                    if (target.value === "." && !operand2.length) {
-                        operand2 += "0";
-                    }
-                    operand2 += target.textContent;
-                } else {
-                    if (resetCalculator) {
-                        operand1 = "";
-                        resetCalculator = false;
-                    }
-                    if (target.value === "." && operand1.includes(".")) return;
-                    if (target.value !== "." && operand1[operand1.length-1] === "0") return;
-                    if (target.value === "." && !operand1.length) {
-                        operand1 += "0";
-                    }
-                    operand1 += target.textContent;
+        if (!target.classList.contains("button")) return;
+        const value = target.value;
+        if (target.classList.contains("digit")) {
+            const currentOperand = operator ? operand2 : operand1;
+
+            if (value === "." && currentOperand.includes(".")) return;
+            if (value !== "." && currentOperand === "0") return;
+            
+            if (operator) {
+                if (value === "." && !operand2.length) {
+                    operand2 += "0";
                 }
-                break;
-            case target.classList.contains("operator"):
-                if (operand1.length && !operator.length) {
-                    operator = target.textContent;
-                } else if (operand1.length && operator.length && operand2.length) {
-                    const result = `${operate(operand1, operator, operand2)}`;
-                    operand1 = result;
-                    operator = target.textContent;
-                    operand2 = "";
+                operand2 += target.textContent;
+            } else {
+                if (newCalculation) resetState();
+                if (value === "." && !operand1.length) {
+                    operand1 += "0";
                 }
-                break;
-            case target.classList.contains("enter"):
-                if (operand1.length && operator.length && operand2.length) {
-                    const result = `${operate(operand1, operator, operand2)}`;
-                    operand1 = result;
-                    operator = "";
-                    operand2 = "";
-                    resetCalculator = true;
-                }
-                break;
-            case target.classList.contains("delete"):
-                const equation = display.textContent.split(" ");
-                const length = equation.length;
-                if (equation[2]) {
-                    operand2 = equation[length-1].slice(0,-1);
-                } else if (equation[1]) {
-                    operator = ""
-                } else if (equation[0]) {
-                    operand1 = equation[length-1].slice(0,-1);
-                }
-                break;
-            case target.classList.contains("clear"):
-                operand1 = "", operator = "", operand2 = "";
-                break;
-            default:
-                console.error("There was an error. Please try again.");
-                break;
+                operand1 += target.textContent;
+            }
+        } else if (target.classList.contains("operator")) {
+            if (operand1.length && operator.length && operand2.length) {
+                operand1 = `${operate(operand1, operator, operand2)}`;
+                operand2 = "";
+            }
+            operator = target.textContent;
+        } else if (target.classList.contains("enter")) {
+            if (operand1.length && operator.length && operand2.length) {
+                operand1 = `${operate(operand1, operator, operand2)}`;
+                operator = "";
+                operand2 = "";
+                newCalculation = true;
+            }
+        } else if (target.classList.contains("delete")) {
+            if (operand2) {
+                operand2 = operand2.slice(0,-1);
+            } else if (operator) {
+                operator = "";
+            } else if (operand1) {
+                operand1 = operand1.slice(0,-1);
+            }
+        } else if (target.classList.contains("clear")) {
+            resetState();
+        } else {
+            console.error("There was an error. Please try again.");
         }
-        updateDisplay(`${operand1}${operator ? " " + operator : ""}${operand2 ? " " + operand2 : ""}`);
+        updateDisplay(getDisplayContent(operand1, operator, operand2));
     });
 
     document.addEventListener("keydown", (event) => {
-        const buttons =document.querySelectorAll(".button");
+        const buttons = document.querySelectorAll(".button");
         buttons.forEach((button) => {
-            if (button.value === event.key) {
+            if (button.value === event.key || (event.key === "Enter" && button.classList.contains("enter"))) {
                 button.click();
             }
         });
